5911c93c06cc47b8e888c19347bc5a5d7a4c18d4
Blake Richey <blake.e.richey@gmail.com>
2021-05-28 16:25:17 -0400

    Added client/listener examples.

--- examples/sockets/client.py
+++ examples/sockets/client.py
@@ -1,12 +1,19 @@
-import sys
 from multiprocessing.connection import Listener, Client
-from blitzen import get_local_ip
 
-address = (get_local_ip(), 57754)
+address = ('192.168.1.68', 50000)
 
 def client():
+    #Get port from endpoint
     conn = Client(address, authkey=b'secret password')
-    print(conn.recv())
+    port = conn.recv()
+    conn.close()
+
+    #Connect to port
+    conn = Client((address[0], int(port)), authkey=b'secret password') #Connects to open port
+
+    #Wait for task to complete
+    delay = conn.recv()
+    print(f'Task took {delay} seconds.')
     conn.close()
 
 if __name__ == '__main__':
--- examples/sockets/server.py
+++ examples/sockets/server.py
@@ -1,14 +1,60 @@
-import sys
+import time
+import random
+import socket
 from multiprocessing.connection import Listener, Client
+from threading import Thread
 
-def server():
-    listener = Listener(authkey=b'secret password', family='AF_INET')
+def get_local_ip():
+  return socket.gethostbyname(socket.gethostname())
+
+def get_free_port():
+    s = socket.socket(socket.AF_INET)
+    s.bind(('localhost', 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
+def socket_specific_task(port,):
+    """
+        Perform a task asynchonously after receiving a connection on the 
+        specified port
+    """
+    address = (get_local_ip(), port)
+    listener = Listener(address, authkey=b'secret password')
     print('Listening on', listener.address)
     conn = listener.accept()
     print('connection accepted from', listener.last_accepted)
-    conn.send('hello')
+
+    delay = random.randint(1,3)
+    time.sleep(delay)
+
+    conn.send(str(delay))
+    
     conn.close()
     listener.close()
+
+def server():
+    address = (get_local_ip(), 50000) #Endpoint located on port 50000
+    listener = Listener(address, authkey=b'secret password')
+    print('Listening on', listener.address)
+    conn = listener.accept()
+    print('connection accepted from', listener.last_accepted)
+
+    port = get_free_port() ##### Port not guarenteed to remain open after this line #####
+
+    #Asynchonously perform task and respond to connections on the free port
+    thread = Thread(
+        target=socket_specific_task,
+        args=(port,)
+    )
+    thread.daemon = True
+    thread.start()
+    
+    conn.send(str(port)) #Send back port to expect results to come from
+    
+    conn.close()
+    listener.close()
+    thread.join()
 
 if __name__ == '__main__':
     server()
